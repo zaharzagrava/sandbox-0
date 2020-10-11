3a43d6dbad8c1101eb4b829b46ddb88ca2477e99
Zahar Zagrava <zaharzagrava@gmail.com>
2020-10-11 14:10:56 +0300

    Prepared build to serve

---
+++ backend/src/clientbuild/precache-manifest.1b34fc362e214539565d3cf94bb19f50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34bc5f0ffab38ccace518653ef9f26de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e01c5472f43c79000b09",
+    "url": "/static/css/2.e6ac38db.chunk.css"
+  },
+  {
+    "revision": "7013f728ce68ac5909d0",
+    "url": "/static/css/main.2b1f759d.chunk.css"
+  },
+  {
+    "revision": "e01c5472f43c79000b09",
+    "url": "/static/js/2.2e542990.chunk.js"
+  },
+  {
+    "revision": "cac620bc00c578fabea0f9f95ef06c5b",
+    "url": "/static/js/2.2e542990.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7013f728ce68ac5909d0",
+    "url": "/static/js/main.ba4ea30b.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
